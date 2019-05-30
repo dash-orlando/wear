7ad39449ee9c0a7783d41e4bc36aa78a68cedce5
NET\flobo <fluvio.lobo@gmail.com>
2019-05-30 12:26:54 -0400

    Began working on the squisher program for Rimma

---
+++ slicing/squisher.py
@@ -0,0 +1,23 @@
+'''
+squisher.py
+
+LAYER SQUISHER
+    - Developed for the WEAR project
+    - Changes the Z heigh of the printer do enhance layer bonding
+
+Authors:
+    - Fluvio Lobo Fengolietto
+    - Rimma Uysal
+'''
+
+# Import/Read GCODE
+
+#gcode = open( filename
+
+
+# Definitions
+
+squish = []
+slayer = []
+
+
