ba399d7dcdb3a6803d7045502b9f2c5523d5c117
NET\flobo <fluvio.lobo@gmail.com>
2019-06-04 15:04:35 -0400

    Completed the first version of the squisher program

--- slicing/squisher.py
+++ slicing/squisher.py
@@ -24,6 +24,8 @@ import  os
 
 # USER MUST CHANGE THIS --------------------------------------------------------------------------- #
 input_filename                  = 'P6.gcode'
+target_layers                   = [3,5,7]
+squish_percentage               = 0.50
 # ------------------------------------------------------------------------------------------------- #
 
 input_filename_wo_extension     = input_filename[ :input_filename.find('.') ]                       # extract input filename without extension
@@ -43,52 +45,88 @@ def read_gcode( filename ):
     with open( filename, 'r' ) as f:
         lines = f.readlines()
 
-    return lines
+    return f, lines
 
+# ------------------------------------------------------------------------------------------------- #
 
-def write_gcode( filename, data ):
+def write_gcode( filename, lines ):
     '''
     Writes GCODE using input array of strings
-
-        TO DO:
-            - Add a check for file existance and erasing to avoid issues/errors
-        
     '''
 
     if os.path.exists( filename ):
-        print( "Output filename exists... will remove to avoid overwriting issues" )
+        print( '> Output filename exists... will remove to avoid overwriting issues' )
         os.remove( filename )
     
     f = open( output_filename, 'a+' )
 
-    for i in range( 0 , len(data) ):
-        f.write( data[i] )
+    Nlines = len(lines)
+    for i in range( 0 , Nlines ):
+        f.write( lines[i] )
 
     f.close()
 
+    return f
+
+# ------------------------------------------------------------------------------------------------- #
+
+def squish_layers( lines, layers, percentage ):
+    '''
+    Reduces selected layers by a specific percentage
+    '''
+
+    Nlines                          = len(lines)                                                    # calculate number of lines in input file/data structure
+
+    for i in range( 0, Nlines ):                                                                    # find layer height
+        if lines[i][4:15] == 'layerHeight':
+            layer_height            = float( lines[i][17:] )
+            print( '> GCODE Layer Height set to = {}'.format( layer_height ) )
+            break
+
+
+    # --------------------------------------------------------------------------------------------- # search for specific layer lines to modify
+    mod_indeces                     = []
+    mod_lines                       = []
+    layer                           = 3
+    for i in range( 0, Nlines ):
+
+        if lines[i][0:7] == '; layer':
+            remove_semi_colon = lines[i].strip(';')
+            split_by_comma = remove_semi_colon.split(',')
+            try:
+                layer_number        = int(split_by_comma[0].split(' ')[2])
+                z_height            = float( split_by_comma[1].split(' ')[3][:-1] )
+            except:
+                pass
+
+            ddp = 0
+            if layer_number >= layer:
+                if layer_number >= 10:
+                    ddp = 1
+                mod_val             =  round( z_height - ( layer_height*percentage ), 4)
+                mod_str             =  '{}{}\n'.format( lines[i][:(15+ddp)], str( mod_val ) )
+                mod_indeces.append( i           )
+                mod_lines.append(   mod_str     )
+
+            
+                print( layer_number, z_height, mod_val )
+            
+        else:
+            mod_lines.append(   lines[i] )
+
+    # --------------------------------------------------------------------------------------------- # output/return
+    return mod_indeces, mod_lines        
 
 # ================================================================================================= #
 # Program
 # ================================================================================================= #
 
 
-lines = read_gcode( input_filename )
+input_file_obj, lines           = read_gcode( input_filename )
 
-data = ['hola','pp','hola']
+mod_indeces, mod_lines = squish_layers( lines, 3, squish_percentage )
 
-write_gcode( output_filename, data )
+write_gcode( output_filename, mod_lines )
 
-
-'''
-gcode = open( filename, 'r' )
-
-print( gcode.read(10) )
-
-
-# Definitions
-
-squish = []
-slayer = []
-'''
 
 
